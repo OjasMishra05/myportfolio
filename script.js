d72af30524425e63bdd6e655d4f77c5eabcd71a6
OjasMishra05 <125395146+OjasMishra05@users.noreply.github.com>
2023-03-08 10:45:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('Running')
+
+let about = document.querySelector('.about')
+let form = document.querySelector('.form')
+let workwithme = document.querySelectorAll('.workwithme')
+
+// Array.from(about).addEventListener('click', ()=>{
+//     console.log('clicked on about')
+// })
+
+// function rotate(){
+//     console.log('clicked on about')
+//     about.style.transform = 'rotate(-10deg)'
+// }
+
+Array.from(workwithme).addEventListener('click', ()=>{
+    console.log('clik]ckede')
+})
